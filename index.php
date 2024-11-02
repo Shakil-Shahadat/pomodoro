38d53f8bfde3e541726b8b6d31e73f73b45feacb
Shakil Shahadat <shakil.regi@gmail.com>
2024-11-02 18:07:12 +0600

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
 	<input id="task" type="text" placeholder="Task Name" size="30">
 
-	<div id="timer">
+	<div id="timer" class="timer">
 		<span id="min" class="min">25</span>:<span id="sec" class="sec">00</span>
 	</div>
 
